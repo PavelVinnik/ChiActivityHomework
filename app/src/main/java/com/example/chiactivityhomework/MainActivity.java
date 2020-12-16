540cdc9bac5a50579c993b5283a5da8b3189b2d7
Pavel Vinnik <vinnik.pavel@gmail.com>
2020-12-16 15:19:42 +0200

    Added NavigationDrawer. Added logging of Activity lifecycle. Navigation Drawer Item text color generated on first launch.

--- app/src/main/java/com/example/chiactivityhomework/MainActivity.java
+++ app/src/main/java/com/example/chiactivityhomework/MainActivity.java
@@ -1,15 +1,115 @@
 package com.example.chiactivityhomework;
 
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "MainActivity";
+    private static final String SHARED_PREFERENCES_NAME = "prefs";
+    private static final String IS_FIRST_LAUNCH_KEY = "isFirstLaunch";
+    private static final String TAB_COLOR_KEY = "tabColorKey";
+
+    DrawerLayout drawer;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "onCreate");
+
         setContentView(R.layout.activity_main);
+
+        if (isFirstLaunch()) {
+            Log.d(TAG, "FirstLaunch");
+            setColorPref();
+        }
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setItemTextColor(ColorStateList.valueOf(getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE).getInt(TAB_COLOR_KEY, Color.BLACK)));
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawer = findViewById(R.id.drawer);
+        drawer.close();
+        return true;
+    }
+
+    private boolean isFirstLaunch() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE);
+        if (sharedPreferences.getBoolean(IS_FIRST_LAUNCH_KEY, true)) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(IS_FIRST_LAUNCH_KEY, false).apply();
+            return true;
+        } else return false;
+    }
+
+    private void setColorPref() {
+        Random rng = new Random();
+        int color = Color.argb(255, rng.nextInt(256), rng.nextInt(256), rng.nextInt(256));
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE);
+        sharedPreferences.edit().putInt(TAB_COLOR_KEY, color).apply();
     }
 }
